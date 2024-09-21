a34bca55bc4840f0806477a8a39c5a2875117d6a
vitaliytv <v@nitra.ai>
2024-09-21 11:30:10 +0100

    gcloud auth

--- prepare-gcloud.sh
+++ prepare-gcloud.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# gcloud auth configure-docker europe-west4-docker.pkg.dev,europe-north1-docker.pkg.dev
-# gcloud auth activate-service-account github-actions@abie-ua.iam.gserviceaccount.com --key-file=/service-account/github-actions.json
+gcloud auth configure-docker europe-west4-docker.pkg.dev,europe-north1-docker.pkg.dev
+gcloud auth activate-service-account github-actions@abie-ua.iam.gserviceaccount.com --key-file=/service-account/github-actions.json
 ./run.sh
