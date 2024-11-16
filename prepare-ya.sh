522c1d801e46c8502b5a03ca2cb32fd480ef3fb3
vitaliytv <v@nitra.ai>
2024-11-16 14:55:33 +0200

    # yc container registry configure-docker

--- prepare-ya.sh
+++ prepare-ya.sh
@@ -5,6 +5,6 @@ yc config set service-account-key /service-account/github-actions.json
 yc config set cloud-id b1g5ld4jd5bkt80mpv6b
 yc config set folder-id b1gh9adr678gi95acvdg
 
-yc container registry configure-docker
+# yc container registry configure-docker
 
 ./run.sh
