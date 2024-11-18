8b40b2cea976f642352ca8f2f43454312a3e4f29
vitaliytv <v@nitra.ai>
2024-11-18 08:59:17 +0200

    init-ya

--- init-ya.sh
+++ init-ya.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp -r -v /home/runner/externals/. /home/runner/tmpDir/
+
 yc config profile create github-actions
 yc config set service-account-key /service-account/github-actions.json
 yc config set cloud-id b1g5ld4jd5bkt80mpv6b
@@ -7,4 +9,3 @@ yc config set folder-id b1gh9adr678gi95acvdg
 
 yc container registry configure-docker
 
-./run.sh
