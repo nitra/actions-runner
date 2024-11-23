3270fe4e2dd24e642560d56a51af418d2fd47104
vitaliytv <v@nitra.ai>
2024-11-23 21:40:42 +0200

    europe-west1-docker.pkg.dev

--- prepare-gcloud.sh
+++ prepare-gcloud.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcloud auth configure-docker europe-west4-docker.pkg.dev,europe-north1-docker.pkg.dev,europe-central2-docker.pkg.dev,us-central1-docker.pkg.dev
+gcloud auth configure-docker europe-west1-docker.pkg.dev,europe-west4-docker.pkg.dev,europe-north1-docker.pkg.dev,europe-central2-docker.pkg.dev,us-central1-docker.pkg.dev
 gcloud auth activate-service-account --key-file=/service-account/github-actions.json
 ./run.sh
